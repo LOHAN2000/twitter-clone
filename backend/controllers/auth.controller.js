192c37cf084d664be47fbe0daa069b97521642fb
LOHAN2000 <anghelovy@outlook.com>
2025-01-04 18:44:19 -0500

    jwt authentication added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,63 @@
+import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js'
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+
 export class AuthController {
+  
   static async signup (req, res) {
-    res.json({data: 'You hit the signup endpoint'});
+    try {
+      const {fullname, username, email, password} = req.body
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+      if (!emailRegex.test(email)) {
+        return res.status(400).json({message: 'Invalid email format'})
+      }
+
+      const existingUser = await User.findOne({ username })
+
+      if (existingUser) {
+        return res.status(400).json({message: 'Username is already taken'})
+      }
+
+      const existingEmail = await User.findOne({ email })
+
+      if (existingEmail) {
+        return res.status(400).json({message: 'Email is already taken'})
+      }
+
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(password, salt)
+
+      const newUser = new User({
+          fullname: fullname,
+          username: username,
+          email: email,
+          password: hashedPassword
+      })
+
+      if (newUser) {
+        generateTokenAndSetCookie(newUser._id, res)
+        await newUser.save()
+
+        res.status(201).json({
+          _id: newUser._id,
+          fullName: newUser.fullname,
+          username: newUser.username,
+          email: newUser.email,
+          followers: newUser.followers,
+          following: newUser.following,
+          profileImg: newUser.profileImg,
+          coverImg: newUser.coverImg
+        })
+      } else {
+        res.status(400).json({'Error': 'Invalid user data'})
+      }
+
+
+    } catch (error) {
+      console.log('Error function signup')
+    }
   }
 
   static async login (req, res) {
--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,13 @@ dontenv.config()
 const app = express()
 const PORT = process.env.PORT ?? 5000
 
+app.use('/api/auth', authRoutes);
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Server is ready')
 })
 
-app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`)
