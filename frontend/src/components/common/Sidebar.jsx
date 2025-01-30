d2cfd1e68821a48d6ea7ee5527fd96440ca9656b
LOHAN2000 <anghelovy@outlook.com>
2025-01-30 17:17:08 -0500

    Refactor Sidebar component to use useLogout hook; update user display logic and improve user data handling with React Query

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,47 +5,18 @@ import { GoHome, GoHomeFill } from 'react-icons/go'
 import { FaRegUser, FaUser  } from "react-icons/fa6";
 import { HiOutlineBell, HiMiniBell  } from "react-icons/hi2";
 import { IoExitOutline } from "react-icons/io5";
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { toast } from 'sonner';
+import { useQuery } from '@tanstack/react-query';
+import { useLogout } from '../../hooks/useLogout';
 
 
 export const Sidebar = () => {
 
+  const { data:authUser } = useQuery({queryKey: ['authUser']})
   const [pageView, setPageView] = useState('home')
+  const { mutate: logout, isPending } = useLogout()
 
-  const queryClient = useQueryClient()
-
-  const { mutate, isPending, error } = useMutation({
-    mutationFn: async () => {
-      try {
-        const response = await fetch('/api/auth/logout', {
-          method: 'POST'
-        })
-
-        if (!response.ok) {
-          const errorData = await response.json()
-          const errorMessage = errorData.message || 'Error interno del servidor'
-          throw new Error(errorMessage)
-        }
-
-        const data = await response.json()
-
-        if (data.error) {
-          throw new Error(data.error)
-        }
-
-        return data
-
-      } catch (error) {
-        toast.error(error.message)
-        throw error
-      }
-    },
-    onSuccess: (data) => {
-      toast.success(data.message)
-      queryClient.invalidateQueries({ queryClient: ['authUser']})
-    }
-  })
+  console.log(authUser)
+  
 
   return (
     <div className='flex-[2_2_0] border-e border-[rgb(47,51,54)]'>
@@ -79,13 +50,13 @@ export const Sidebar = () => {
         </div>
         <div className='flex flex-row items-center rounded-full hover:bg-[rgb(231,233,234,0.1)] gap-x-3 justify-between px-2 py-1'>
           <div className='flex flex-row items-center gap-x-3'>
-            <img src='/Twitter_default_profile_400x400.png' className='object-container w-1/5 h-1/5 rounded-full'/>
+            <img src={authUser.User.coverImg || '/Twitter_default_profile_400x400.png'} className='object-container w-1/5 h-1/5 rounded-full'/>
             <div className='flex flex-col w-full '>
-              <h1 className='text-sm md:text-base'>Nombre</h1>
-              <h1 className='text-sm md:text-base text-gray-600'>UserName</h1>
+              <h1 className='text-sm md:text-md'>{authUser.User.username}</h1>
+              <h1 className='text-sm md:text-base text-gray-600'>{authUser.User.fullname}</h1>
             </div>
           </div>
-          <IoExitOutline onClick={() => mutate()} className='w-2/4 h-2/4'/>
+          <IoExitOutline onClick={() => logout()} className='w-2/4 h-2/4'/>
         </div>
       </div>
     </div>
